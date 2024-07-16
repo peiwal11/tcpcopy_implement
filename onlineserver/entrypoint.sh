bc21cb646fe815a115e3832403f84ee5de5606d9
peiwal11 <walmarthaventbeen@gmail.com>
2024-07-16 10:56:31 +0800

    fix: ensure assistant server could correctly identify the traffic from target server

--- onlineserver/entrypoint.sh
+++ onlineserver/entrypoint.sh
@@ -2,6 +2,6 @@
 
 python3 /usr/local/bin/app.py &
 
-/usr/local/tcpcopy/sbin/tcpcopy -x 9999-10.191.7.16:10231 -s 10.191.7.16 -c 10.190.4.137
+/usr/local/tcpcopy/sbin/tcpcopy -x 9999-10.191.7.16:10231 -s 10.191.7.16 
 	
 
