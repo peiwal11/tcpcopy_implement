75f816c05f35c0d457fe9ef7e5b4fc9fcf527059
peiwal11 <walmarthaventbeen@gmail.com>
2024-07-17 13:51:55 +0800

    fix: env shouldn't be hard-coded

--- onlineserver/entrypoint.sh
+++ onlineserver/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/usr/local/tcpcopy/sbin/tcpcopy -x 9999-10.191.7.16:10231 -s 10.191.7.16 
+/usr/local/tcpcopy/sbin/tcpcopy -x $service_port-$target_server_IP:$target_server_service_port -s $assistant_server_IP 
 	
 
